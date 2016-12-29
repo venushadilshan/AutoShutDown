3b698838f7909ed3c4710510c09fa3bf6b7d3720
venushadilshan <venusha125@gmail.com>
2016-12-29 09:38:08 +0530

    AutoShutDown
    PC auto shut downer. For the user given time

---
+++ AutoShutDowner/AutoShutDowner/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace AutoShutDowner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Shut down after :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(106, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Proceed";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(106, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(212, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "minutes";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(106, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 20);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Abort Shut down";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(106, 119);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 20);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Hide";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 152);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auto Shut Downer by Venusha Dilshan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ AutoShutDowner/AutoShutDowner/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoShutDowner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { // converting the textbox string to int
+
+            int time = Convert.ToInt32(this.textBox1.Text);
+            // converting sec to mins by * 60. yy is the time in minutes
+            int yy=time * 60;
+
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = "/C  shutdown /s /t " + yy;
+            process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.Start();
+            string q = "";
+            while (!process.HasExited)
+            {
+               // progressBar1.Value = 20;
+                q += process.StandardOutput.ReadToEnd();
+            }
+          //  label1.Text = q;
+            //  MessageBox.Show(q); 
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = "/C shutdown /a ";
+            process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.Start();
+            string q = "";
+            while (!process.HasExited)
+            {
+                // progressBar1.Value = 20;
+                q += process.StandardOutput.ReadToEnd();
+            }
+          
+            //  MessageBox.Show(q); 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {    // convertign to int
+            int y = Convert.ToInt32(this.textBox1.Text);
+            // secs >>> to mins
+            int x = y * 60;
+        MessageBox.Show("The Window will be Hidden and Your PC will be shut down after " + x + " minutes");
+        this.Hide();
+        }
+    }
+}
